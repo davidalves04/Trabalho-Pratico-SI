3d7a9861f02eda0149f78328825b66f3a6518f3c
paratyy <pedro.af.paraty@gmail.com>
2025-05-31 11:58:07 +0100

    Update main.py

--- main.py
+++ main.py
@@ -9,9 +9,12 @@ def menu():
     print("0. Sair")
 
 def encrypt_file(input_file, output_file, password):
+    folder = "txt files"
+    input_path = os.path.join(folder, input_file)
+    output_path = os.path.join(folder, output_file)
     key = password.ljust(16)[:16].encode()
     
-    with open(input_file, 'r', encoding='utf-8') as f:
+    with open(input_path, 'r', encoding='utf-8') as f:
         text = f.read()
 
     iv = os.urandom(16)
@@ -22,15 +25,18 @@ def encrypt_file(input_file, output_file, password):
 
     encrypted_data = cipher.encrypt(padded_text.encode())
 
-    with open(output_file, 'w', encoding='utf-8') as f:
+    with open(output_path, 'w', encoding='utf-8') as f:
         f.write(base64.b64encode(iv + encrypted_data).decode())
 
     print(f"Ficheiro '{input_file}' encriptado com sucesso para '{output_file}'.")
 
 def decrypt_file(input_file, output_file, password):
+    folder = "txt files"
+    input_path = os.path.join(folder, input_file)
+    output_path = os.path.join(folder, output_file)
     key = password.ljust(16)[:16].encode()
 
-    with open(input_file, 'r', encoding='utf-8') as f:
+    with open(input_path, 'r', encoding='utf-8') as f:
         encrypted_b64 = f.read()
 
     encrypted_data = base64.b64decode(encrypted_b64)
@@ -56,7 +62,7 @@ def decrypt_file(input_file, output_file, password):
     pad_len = decrypted_data[-1]
     decrypted_text = decrypted_data[:-pad_len].decode()
 
-    with open(output_file, 'w', encoding='utf-8') as f:
+    with open(output_path, 'w', encoding='utf-8') as f:
         f.write(decrypted_text)
 
     print(f"\nFicheiro '{input_file}' desencriptado com sucesso para '{output_file}'.")
